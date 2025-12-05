0319a34a0dae570c2f4b40eb2256fca7871e2341
Igor <igoremanuelteixeira@gmail.com>
2025-12-05 13:01:17 -0300

    Atualização do código com ajustes na API e cronômetro

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -5,15 +5,17 @@ class NewsService {
   final String apiKey = "1300a7e5d78f4307a495dc1c1b5208ea";
 
   Future<List<dynamic>> getTopHeadlines() async {
+    // Busca ampla, sem filtro de país ou idioma
     final url = Uri.parse(
-      "https://newsapi.org/v2/top-headlines?country=br&language=pt&apiKey=$apiKey",
+      "https://newsapi.org/v2/everything?q=news&apiKey=$apiKey",
     );
 
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return data["articles"];
+      final articles = data["articles"] as List<dynamic>? ?? [];
+      return articles;
     } else {
       throw Exception("Erro ao carregar notícias");
     }
